df302eb079dc7b14075e45d83acf187756cbb4b2
Narek Ghevandiani <n.ghevandiani@gmail.com>
2019-04-04 12:21:30 +0400

    Change email form to div, add form field validation

--- mail.js
+++ mail.js
@@ -1,8 +1,26 @@
 let onMailFormSubmit = () => {
-  const name = contactform.name.value;
-  const email = contactform.email.value;
-  const subject = contactform.subject.value;
-  const msg = contactform.comments.value;
+
+  const nameElem = document.getElementById('name');
+  const emailElem = document.getElementById('email');
+  const subjectElem = document.getElementById('subject');
+  const msgElem = document.getElementById('comments');
+
+  let validated = true;
+  [nameElem, emailElem, subjectElem, msgElem].forEach(elem => {
+    if (!elem.value) {
+      elem.setAttribute('style', 'border-color: #ef5350');
+      validated = false;
+    }
+  });
+
+  if (!validated) {
+    return;
+  }
+
+  const name = nameElem.value;
+  const email = emailElem.value;
+  const subject = subjectElem.value;
+  const msg = msgElem.value;
 
   fetch('https://dohirus-mail-service.herokuapp.com/mail', {
     method: 'POST',
